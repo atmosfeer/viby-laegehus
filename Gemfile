8ddd712a502a53b3228b857a8c62558d46ababd4
Nicolas Feer <atmosfeer@gmail.com>
2016-06-19 12:28:04 +0200

    Added Google Analytics

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ gem 'middleman', '~> 3.4'
 # Deploy to GitHub Pages
 gem 'middleman-deploy', '~> 1.0'
 
+# Google Analytics
+gem 'middleman-google-analytics', '~> 2.1'
+
+
 # Assets
 gem 'jquery-middleman'
 gem 'bootstrap-sass', '3.3.6'
--- config.rb
+++ config.rb
@@ -91,3 +91,9 @@ activate :deploy do |deploy|
   # deploy.strategy = :submodule # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message' # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
+
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-79561762-1' # Replace with your property ID.
+end
+
+
